c74b8e5fcc6252eef9e621755ca96d88ff73f4f9
PaulaDanthu <danthupaula@gmail.com>
2023-05-12 13:21:00 +0200

    component popup removed

---
+++ src/components/Popup.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="popup" v-if="isPopupOpen"
+        class="
+        w-96
+        absolute
+        z-20
+        m-52
+        bg-white
+        text-center
+        p-8
+    ">
+        <button @click="closePopup()"
+            class="
+            bg-blue-500
+            hover:bg-blue-300
+            py-4
+            px-8
+        ">Close</button>
+        <h1 
+            class="
+            mb-12
+            mt-12
+        ">Cookies</h1>
+        <p 
+            class="
+            mb-12
+            mt-12
+        ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi error non quam dicta voluptate harum sunt quas repellendus recusandae repellat ullam, ab quo quasi eveniet minus! At nisi sunt hic?</p>
+    </div>
+</template>
+
+<script>
+
+
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,35 @@
         flex-col-reverse
         items-center
     ">
-        <div>
+    <div id="popup" v-if="isPopupOpen"
+        class="
+        w-96
+        absolute
+        z-20
+        m-52
+        bg-white
+        text-center
+        p-8
+    ">
+        <button @click="closePopup"
+            class="
+            bg-blue-500
+            hover:bg-blue-300
+            py-4
+            px-8
+        ">Close</button>
+        <h1 
+            class="
+            mb-12
+            mt-12
+        ">Cookies</h1>
+        <p 
+            class="
+            mb-12
+            mt-12
+        ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi error non quam dicta voluptate harum sunt quas repellendus recusandae repellat ullam, ab quo quasi eveniet minus! At nisi sunt hic?</p>
+    </div>
+    <div>
             <img src="../assets/mx5mazdamain.jpeg" alt="Mazda MX5 home page" class="
             w-screen
         ">
@@ -203,14 +231,29 @@
 </template>
 
 <script>
+import { ref } from 'vue' 
+
 import Footer from '../components/Footer.vue'
 
+const isPopupOpen = ref(true)
+
 export default {
-    name: 'App',
+    setup() {
+        const isPopupOpen = ref(true)
+
+        const closePopup = () => {
+            this.isPopupOpen = false
+        }
+        return {
+            isPopupOpen, closePopup
+        }
+    },
+
     components: {
         'my-footer' : Footer
     }
-}
+    }
+
 </script>
 
 <style>
