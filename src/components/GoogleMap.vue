268e614f1d91d25fe3900e137bb0c97aa0621f6b
PaulaDanthu <danthupaula@gmail.com>
2023-05-27 19:27:48 +0200

    google maps setup

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,14 +1,28 @@
 <template>
-    <GMapMap :center="{lat: 51.093048, lng: 6.842120}" :zoom="7" ref="myMapRef" :disableDefaultUI="true"
-    style="height: 20rem; width: 100vw;" />
+    <GoogleMap api-key="AIzaSyC3V_2iMFBIbhaGaSi1Y06bCeqAfo-MmFs" class="map" :center="center" :zoom="15">
+        <Marker :options="{ position: center }" />
+    </GoogleMap>
 </template>
 
 <script>
+import { defineComponent } from "vue";
+import { GoogleMap, Marker } from "vue3-google-map";
 
+export default defineComponent({
+    components: { GoogleMap, Marker },
+    setup() {
+        const center = { lat: 45.0935173034668, lng: 5.797624111175537 };
 
-export default {
-    components: {
-    
-    }
-}
+        return { center };
+    },
+});
 </script>
+<style>
+.map {
+    width: 100%; 
+    height: 500px;
+    padding-left: 5rem;
+    padding-right: 5rem;
+    margin-top: 4rem;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
-import VueGoogleMaps from '@fawmi/vue-google-maps'
-
-
 
 createApp(App)
-.use(router, VueGoogleMaps, {
-    load: {
-        key: "API_KEY"
-    }
-    })
+.use(router)
 .mount('#app')
